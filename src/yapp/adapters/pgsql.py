5012290ff98cbe04db7353a79a07b01e4c071d55
Carlo Antonio Venditti <c.a.venditti@pm.me>
2022-04-17 16:07:57 +0200

    Remove sqlachemy dependecy — #1

--- src/yapp/adapters/pgsql.py
+++ src/yapp/adapters/pgsql.py
@@ -1,15 +1,27 @@
-from sqlalchemy import create_engine
+from psycopg2 import connect
 
 from .sql import SqlInput, SqlOutput
 
 
 class PgSqlInput(SqlInput):
     def __init__(self, *, username, password, host, port, database, schema=None, where_clause=None):
-        conn_string = f"postgresql://{username}:{password}@{host}:{port}/{database}"
-        super().__init__(create_engine(conn_string), schema=schema, where_clause=where_clause)
+        connection = connect(
+            user=username,
+            password=password,
+            host=host,
+            port=port,
+            dbname=database
+        )
+        super().__init__(connection, schema=schema, where_clause=where_clause)
 
 
 class PgSqlOutput(SqlOutput):
     def __init__(self, *, username, password, host, port, database, schema=None):
-        conn_string = f"postgresql://{username}:{password}@{host}:{port}/{database}"
-        super().__init__(create_engine(conn_string), schema=schema)
+        connection = connect(
+            user=username,
+            password=password,
+            host=host,
+            port=port,
+            dbname=database
+        )
+        super().__init__(connection, schema=schema)
