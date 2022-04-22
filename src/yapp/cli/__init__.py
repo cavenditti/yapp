b38bc3f32176822f2006f090db53cd0ad606cfa5
Carlo Antonio Venditti <c.a.venditti@pm.me>
2022-04-22 08:48:12 +0200

    Moved logs code in separate file
    Added cli option to enable colored output

--- src/yapp/cli/__init__.py
+++ src/yapp/cli/__init__.py
@@ -3,7 +3,6 @@ import yaml
 import graphlib
 import importlib.util
 import os
-import sys
 import types
 import inspect
 import re
@@ -13,6 +12,7 @@ from types import MethodType
 from yapp.core import Pipeline, Job, Inputs
 from yapp import ConfigurationError
 from yapp.cli.validation import pipelines_schema_validation
+from yapp.cli.logs import setup_logging
 
 
 def env_constructor(loader, node):
@@ -73,7 +73,7 @@ def build_job(pipeline_name, step):
     try:
         module = load_module(pipeline_name, step)
         func_name = "execute"
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         # maybe it's a function in module, try also loading that
         if "." in step:
             module_name, func_name = step.rsplit(".", 1)
@@ -208,7 +208,6 @@ def build_inputs(pipeline_name, cfg_inputs, cfg_expose):
 
     for expose_def in cfg_expose:
         logging.debug(f'<expose> parsing "{expose_def}"')
-        it = iter(expose_def)
         key = next(iter(expose_def))
         for item in expose_def[key]:
             exposed_var = next(iter(item))
@@ -284,8 +283,6 @@ def yaml_read(path):
     with open(path, "r") as file:
         parsed = yaml.full_load(file)
     return parsed
-
-
 
 
 def create_pipeline(
@@ -385,133 +382,22 @@ def main():
     )
 
     parser.add_argument(
-        "-l",
-        "--loglevel",
-        nargs="?",
-        dest="loglevel",
-        default="INFO",
-        help="Log level to use",
+        '--color',
+        action="store_const", dest="color", const=True,
+        default=False,
+        help='Print colored output for logs'
     )
 
-    parser.add_argument("pipeline", type=str, help="Pipeline name")
+    parser.add_argument(
+        'pipeline',
+        type=str,
+        help='Pipeline name'
+    )
 
     args = parser.parse_args()
-
     loglevel = args.loglevel.upper()
 
-    logger = logging.getLogger()
-
-    def addLoggingLevel(levelName, levelNum, methodName=None):
-        """
-        Comprehensively adds a new logging level to the `logging` module and the
-        currently configured logging class.
-
-        `levelName` becomes an attribute of the `logging` module with the value
-        `levelNum`. `methodName` becomes a convenience method for both `logging`
-        itself and the class returned by `logging.getLoggerClass()` (usually just
-        `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
-        used.
-
-        To avoid accidental clobberings of existing attributes, this method will
-        raise an `AttributeError` if the level name is already an attribute of the
-        `logging` module or if the method name is already present 
-
-        Example
-        -------
-        >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
-        >>> logging.getLogger(__name__).setLevel("TRACE")
-        >>> logging.getLogger(__name__).trace('that worked')
-        >>> logging.trace('so did this')
-        >>> logging.TRACE
-        5
-
-        """
-        if not methodName:
-            methodName = levelName.lower()
-
-        if hasattr(logging, levelName):
-           raise AttributeError('{} already defined in logging module'.format(levelName))
-        if hasattr(logging, methodName):
-           raise AttributeError('{} already defined in logging module'.format(methodName))
-        if hasattr(logging.getLoggerClass(), methodName):
-           raise AttributeError('{} already defined in logger class'.format(methodName))
-
-        # This method was inspired by the answers to Stack Overflow post
-        # http://stackoverflow.com/q/2183233/2988730, especially
-        # http://stackoverflow.com/a/13638084/2988730
-        def logForLevel(self, message, *args, **kwargs):
-            if self.isEnabledFor(levelNum):
-                self._log(levelNum, message, args, **kwargs)
-        def logToRoot(message, *args, **kwargs):
-            logging.log(levelNum, message, *args, **kwargs)
-
-        logging.addLevelName(levelNum, levelName)
-        setattr(logging, levelName, levelNum)
-        setattr(logging.getLoggerClass(), methodName, logForLevel)
-        setattr(logging, methodName, logToRoot)
-
-    addLoggingLevel('OK', 21)
-    addLoggingLevel('PRINT', 22)
-
-    class LogFormatter(logging.Formatter):
-        width = 26
-
-        white = "\x1b[1;37m"
-        gray = "\x1b[38;5;247m"
-        yellow = "\x1b[1;33m"
-        blue = "\x1b[1;34m"
-        purple = "\x1b[1;35m"
-        green = "\x1b[1;32m"
-        red = "\x1b[31;1m"
-        reset = "\x1b[0m"
-
-        FORMATS = {
-            logging.DEBUG: white,
-            logging.INFO: blue,
-            logging.OK: green,
-            logging.PRINT: gray,
-            logging.WARNING: yellow,
-            logging.ERROR: red,
-            logging.CRITICAL: red
-        }
-
-        def format(self, record):
-            # An ugly hack to prevent printing empty lines from print calls (1/3)
-            if not str(record.msg).strip().strip('\n'):
-                return ''
-            if len(record.module) > 10:
-                record.module = record.module[:10] + '…'
-            if len(record.funcName) > 10:
-                record.funcName = record.funcName[:10] + '…'
-            levelname = record.levelname[:1]
-            lineno = str(record.lineno)
-            head = "%s %s.%s" % (levelname[:1], record.module, record.funcName)
-            head = '[' + head.ljust(self.width-len(lineno)) + ' ' + lineno + ']'
-            record.msg = str(record.msg)
-            if record.msg.startswith('> '):
-                head = self.FORMATS.get(record.levelno) + head
-                record.msg = record.msg[2:] + self.reset
-            else:
-                head = self.FORMATS.get(record.levelno) + head + self.reset
-
-            # An ugly hack to prevent printing empty lines from print calls (2/3)
-            return "%s %s\n" % (head, record.msg)
-
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    # An ugly hack to prevent printing empty lines from print calls (3/3)
-    ch.terminator = ""
-    ch.setLevel(getattr(logging, loglevel))
-    formatter = LogFormatter()
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    # send print calls from Jobs and Hooks to log.
-    # Even though there are probably better ways of doing this,
-    # I prefer this one because keeps the track of where print is called
-    # the downside is that the prints are messed up and splitted
-    sys.stdout.write = logger.print
-
+    setup_logging(loglevel, color=args.color)
 
     try:
         pipeline = create_pipeline(args.pipeline, path=args.path)
