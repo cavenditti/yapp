8092326d7243467c4a5435dac63877b58794f420
Carlo Antonio Venditti <c.a.venditti@pm.me>
2022-04-23 16:19:39 +0200

    Fixed pandas adapters

--- src/yapp/adapters/pandas.py
+++ src/yapp/adapters/pandas.py
@@ -12,14 +12,13 @@ class FunctionWrapperInputAdapter(InputAdapter):
         self.args = args
         self.kwargs = kwargs
 
-    # FIXME endless recursion because of redefinition of __getattr__ in InputAdapter
     def get(self, name):
-        return self.fn(name, *self.args, **self.kwargs)
+        return self.__class__.fn(name, *self.args, **self.kwargs)
 
 
 for fn_name in [a for a in dir(pd) if a.startswith("read_")]:
     fn = pd.__getattribute__(fn_name)
 
-    Adapter = type(fn_name, (FunctionWrapperInputAdapter), {"fn": fn})
+    Adapter = type(fn_name, (FunctionWrapperInputAdapter,), {"fn": fn})
 
     setattr(module, fn_name, Adapter)
